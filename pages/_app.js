66d82776c620e21c3d1e68acc6fbfe3ff0fc89ed
Kingsley <divine10646@gmail.com>
2022-08-22 15:00:20 +0100

    trying to fix css conflict

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
+
 import "bootstrap/dist/css/bootstrap.css";
 import { useEffect } from "react";
-import "../styles/globals.css";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
